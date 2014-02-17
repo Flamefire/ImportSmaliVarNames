5b81a4c0339c957d62771cb08183dbec417411c3
Flamefire <alex@grundis.de>
2014-02-17 15:23:56 +0100

    Final changes to have a working product

--- src/com/flamefire/importsmalinames/handlers/RenameVariablesHandler.java
+++ src/com/flamefire/importsmalinames/handlers/RenameVariablesHandler.java
@@ -54,6 +54,10 @@ import javax.swing.JFileChooser;
 public class RenameVariablesHandler extends AbstractHandler {
     private final static QualifiedName LASTFOLDER = new QualifiedName("flamefire", "LASTFOLDER");
 
+    public RenameVariablesHandler() {
+        super();
+    }
+
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
         Shell shell = HandlerUtil.getActiveShell(event);
@@ -105,19 +109,27 @@ public class RenameVariablesHandler extends AbstractHandler {
     }
 
     private void doRename(Shell shell, ICompilationUnit cu) {
+        IJavaProject proj = getProject(cu);
+        String cClassName = null;
+        String pName = null;
         try {
-            System.out.println("Class: " + cu.getCorrespondingResource().getName());
+            cClassName = cu.getCorrespondingResource().getName();
+            if (proj != null)
+                pName = proj.getElementName();
         } catch (JavaModelException e) {
             e.printStackTrace();
         }
+        if (cClassName == null || pName == null || cClassName.length() == 0 || pName.length() == 0) {
+            MessageDialog.openError(shell, "Error", "Could not access project and class names");
+            return;
+        }
+        boolean classOnly = MessageDialog.openQuestion(shell, "Apply smali names only to current class?",
+                "Do you want to apply the names only to current java file (" + cClassName + ") ->YES\n"
+                        + "Or to the whole project (" + pName + ") -> NO");
         String lastFolder = null;
-        IJavaProject proj = getProject(cu);
         IResource res = null;
         try {
-            if (proj != null)
-                res = proj.getCorrespondingResource();
-            else
-                res = cu.getCorrespondingResource();
+            res = proj.getCorrespondingResource();
             lastFolder = com.flamefire.importsmalinames.utils.Util.getPersistentProperty(res, LASTFOLDER);
         } catch (JavaModelException e) {
         }
@@ -132,9 +144,47 @@ public class RenameVariablesHandler extends AbstractHandler {
                 .getAbsolutePath());
         File smaliFolder = j.getSelectedFile();
         RefactoringController controller = new RefactoringController();
-        if (!controller.init(smaliFolder))
+        if (!controller.init(smaliFolder)) {
+            MessageDialog.openError(shell, "Error", "Could not parse smali classes");
             return;
-        controller.renameVariablesInFile(cu);
-    }
+        }
+        String msg = "This will apply the smali names to the ";
+        if (classOnly)
+            msg += "java file " + cClassName;
+        else
+            msg += "whole project " + pName;
+        msg += "\n\nPlease note that you HAVE to check the changes without a preview and it is better to apply this to the decompiled source before you do any changes yourself.";
+        msg += "\nThis might fail under certain circumstances e.g. with nested classes and certain name combinations.";
+        msg += "\n\nProceed?";
+        if (!MessageDialog.openConfirm(shell, "Apply smali names", msg))
+            return;
+        if (classOnly) {
+            if (!controller.renameVariablesInFile(cu)) {
+                MessageDialog.openError(shell, "Error", "Applying the changes failed. Please check output.");
+                return;
+            }
+        } else {
+            try {
+                IPackageFragment[] packages = proj.getPackageFragments();
+                for (IPackageFragment mypackage : packages) {
+                    if (mypackage.getKind() != IPackageFragmentRoot.K_SOURCE)
+                        continue;
+                    for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+                        if (!controller.renameVariablesInFile(unit)) {
+                            if (!MessageDialog.openConfirm(shell, "Error in "
+                                    + unit.getCorrespondingResource().getName(), "Applying the changes to "
+                                    + unit.getCorrespondingResource().getName()
+                                    + " failed. Please check output.\n\nContinue?"))
+                                return;
+                        }
+                    }
+                }
+            } catch (JavaModelException e) {
+                e.printStackTrace();
+            }
+        }
 
+        MessageDialog.openInformation(shell, "Success",
+                "Names were changed. Please have a look at the output in the console for warnings and errors");
+    }
 }
